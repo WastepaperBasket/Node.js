3f0ec8e10c1c687ae2cfdd12a6616d1c198a5ece
DoYoung Park <cym7356@naver.com>
2023-03-15 22:14:54 +0900

    개발자는 터미널부터 친해져야한다.

---
+++ 설치 커널ver.sh
@@ -0,0 +1,56 @@
+PS C:\Users\G U C C I\Desktop\Node.js> node -v
+v16.17.1
+PS C:\Users\G U C C I\Desktop\Node.js> npm init
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help init` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg>` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
+package name: (node)
+version: (1.0.0)
+description:
+entry point: (index.js) server.js <- 요기에 쓰고싶은 이름 쓰셈 
+test command:
+git repository:
+keywords:
+author:
+license: (ISC)
+About to write to C:\Users\G U C C I\Desktop\Node.js\package.json:
+
+{
+  "name": "node",
+  "version": "1.0.0",
+  "description": "",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
+
+
+Is this OK? (yes)
+npm notice 
+npm notice New major version of npm available! 8.15.0 -> 9.6.1
+npm notice Changelog: https://github.com/npm/cli/releases/tag/v9.6.1
+npm notice Run npm install -g npm@9.6.1 to update!
+npm notice
+PS C:\Users\G U C C I\Desktop\Node.js> 
+PS C:\Users\G U C C I\Desktop\Node.js>
+PS C:\Users\G U C C I\Desktop\Node.js>
+PS C:\Users\G U C C I\Desktop\Node.js>
+PS C:\Users\G U C C I\Desktop\Node.js> npm install express
+
+added 57 packages, and audited 58 packages in 3s
+
+7 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\G U C C I\Desktop\Node.js> 
